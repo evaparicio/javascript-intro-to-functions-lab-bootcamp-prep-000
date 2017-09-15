47cd0f12b9a7fd866fd3af02eca1b0aba7a41165
evaparicio <ev.aparicio.c@gmail.com>
2017-09-15 05:26:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function sayHiToGrandma(string){
   return "I can't hear you!"
 }else if ('HELLO') {
   console.log ("YES, INDEED!")
+  return "YES, INDEED!"
 }
 else {
   return "I love you, too."
