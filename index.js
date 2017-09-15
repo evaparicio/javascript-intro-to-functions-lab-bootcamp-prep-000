317dc28a2b0d8cd2b739d875fcd91d1ac55a0a4c
evaparicio <ev.aparicio.c@gmail.com>
2017-09-15 05:19:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,10 @@ function sayHiToGrandma(string){
   console.log ('hello')
   return "I can't hear you!"
   }
+function sayHiToGrandma(string){
+  console.log('HELLO')
+  return "YES, INDEED!"
+}
+function sayHiToGrandma(string) {
+  console.log('I love you, Grandma.')
+  return "I love you, too."}
